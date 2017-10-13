65d0c40136bd481a54fea0568a2e19a096093b34
Afroza Yasmin <afrozayasmin@Afrozas-MacBook-Pro.local>
2017-10-13 20:14:41 +0600

    Initial Commit

---
+++ hello-world.go
@@ -0,0 +1,15 @@
+/*
+* Run this example from terminal by
+* using this command - go run hello-world.go
+*
+* Output:
+	Hello World!
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ values.go
@@ -0,0 +1,31 @@
+/*
+* Run this example from terminal by
+* using this command - go run hello-world.go
+*
+* Output:
+	golang
+	1+1 =  2
+	7.0/3.0 = 2.3333333333333335
+	false
+	true
+	false
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 = ", 1+1)
+
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+
+	fmt.Println(true || false)
+
+	fmt.Println(!true)
+}
